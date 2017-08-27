fb78c039c9156aa0c228d9efea962f6d304c2e1f
Sebastian Kalicki <sebastian@kalicki.email>
2017-08-27 21:19:27 +0200

    Adjusted direction class.

--- src/base-tabs.component.ts
+++ src/base-tabs.component.ts
@@ -1,23 +1,27 @@
 import { Component, ContentChildren, Input, QueryList } from '@angular/core';
-import { BaseTabComponent } from "./base-tab.component";
+import { BaseTabComponent } from './base-tab.component';
 
 @Component({
-  selector: 'base-tabs',
-  templateUrl: './base-tabs.component.html',
-  styleUrls: ['./base-tabs.component.scss']
+    selector: 'base-tabs',
+    templateUrl: './base-tabs.component.html',
+    styleUrls: ['./base-tabs.component.scss']
 })
 export class BaseTabsComponent {
 
-  @Input() direction: string;
-  @ContentChildren(BaseTabComponent) tabs: QueryList<BaseTabComponent>;
+    @Input() direction: string;
+    @ContentChildren(BaseTabComponent) tabs: QueryList<BaseTabComponent>;
 
-  ngAfterContentInit() {
-    this.tabs.first.isVisible = true;
-  }
+    public classes = {
+        horizontal: this.direction !== 'vertical',
+        vertical: this.direction === 'vertical'
+    };
 
-  public onClick(tab): void {
-    this.tabs.toArray().forEach(tab => tab.isVisible = false);
-    tab.isVisible = true;
-  }
+    ngAfterContentInit() {
+        this.tabs.first.isVisible = true;
+    }
 
+    public onClick(tab): void {
+        this.tabs.toArray().forEach(tab => tab.isVisible = false);
+        tab.isVisible = true;
+    }
 }
