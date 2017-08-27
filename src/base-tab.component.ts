4e4b58400de5761283b10b7af8a616d384f59eb5
Sebastian Kalicki <sebastian@kalicki.email>
2017-08-27 21:19:51 +0200

    Reformat code.

--- src/base-tab.component.ts
+++ src/base-tab.component.ts
@@ -1,18 +1,18 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'base-tab',
-  templateUrl: './base-tab.component.html',
-  styleUrls: ['./base-tab.component.scss']
+    selector: 'base-tab',
+    templateUrl: './base-tab.component.html',
+    styleUrls: ['./base-tab.component.scss']
 })
 export class BaseTabComponent {
 
-  public isVisible: boolean;
+    public isVisible: boolean;
 
-  @Input() title: string;
+    @Input() title: string;
 
-  constructor() {
-    this.isVisible = false;
-  }
+    constructor() {
+        this.isVisible = false;
+    }
 
 }
--- src/base-tabs.component.spec.ts
+++ src/base-tabs.component.spec.ts
@@ -1,25 +1,25 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { BaseTabsComponent } from './base-tabs.component';
+import {BaseTabsComponent} from './base-tabs.component';
 
 describe('BaseTabsComponent', () => {
-  let component: BaseTabsComponent;
-  let fixture: ComponentFixture<BaseTabsComponent>;
+    let component: BaseTabsComponent;
+    let fixture: ComponentFixture<BaseTabsComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BaseTabsComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [BaseTabsComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(BaseTabsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(BaseTabsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
